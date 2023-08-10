e7b2499a6e767c5b46c5f258eb2c3a524f2c762a
thenerd31 <aswinsurya@gmail.com>
2023-08-10 14:41:38 -0700

    changed bio

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -11,9 +11,8 @@ const AboutUs = () => {
         
         <p>
         Aswin Surya is an Indian-American teen researcher, developer, and advocate for those hard of hearing. 
-        Driven by his own personal experience with microtia atresia, a condition that led to hearing loss, 
-        Aswin's passion for STEM and AI fuels his ambition to create innovative solutions. Currently a senior 
-        at Bellarmine College Prep in California, his work is focused on integrating technology with empathy, 
+        Driven by his own experiences and challenges, Aswin's passion for AI fuels his ambition to create innovative solutions within healthcare.
+        Currently a senior at Bellarmine College Prep in California, his work is focused on integrating technology with empathy, 
         aiming to bridge the gap between the hearing and the deaf communities. Aswin's resilience and commitment 
         to his goals inspire his continual pursuit of excellence in the field of science and technology. 
         </p>
